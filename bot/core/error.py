e033bae6f5e42a23225cbcd07fdcffb2a9bf2687
arcinfini <arcinfini@DESKTOP-TE9G52V>
2025-04-08 11:45:54 -0400

    inclusion of error and util

---
+++ bot/core/error.py
@@ -0,0 +1,33 @@
+from discord import Embed, Interaction
+from discord.ext import commands
+
+
+class InternalError(Exception):
+    """An internal error that servers as a wrapper around discord.py errors.
+
+    Extends
+    -------
+    `discord.ext.commands.CommandError`
+    `discord.app_commands.AppCommandError`
+    """
+
+    title: str = "Internal Error"
+    content: str = (
+        "an unhandle error occured. if this continues, submit a ticket."
+    )
+    color: int = 0xC6612A
+
+    def __init__(self, *, error: Exception | None = None):
+        self.cause = error
+
+    def format_embed(self, info: commands.Context | Interaction) -> Embed:
+        """Build an embed to be sent with error information."""
+        return Embed(
+            title=self.title,
+            description=self.content.format(info=info),
+            color=self.color,
+        )
+
+    async def alert(self, info: commands.Context | Interaction) -> None:
+        """Alert the bot dev channel of an exception."""
+        # TODOa
---
+++ bot/util/__init__.py
@@ -0,0 +1,14 @@
+from enum import Enum
+from typing import Self
+
+
+class Mode(Enum):
+    """The modes the program can be ran in."""
+
+    DEV = 0
+    PROD = 10
+
+    @classmethod
+    def from_str(cls, label: str) -> Self:
+        """Transform a string into an enum value."""
+        return cls[label.upper()]
